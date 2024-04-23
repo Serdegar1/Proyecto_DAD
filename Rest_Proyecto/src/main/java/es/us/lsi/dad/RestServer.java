8b114f3fd98aeec01254877b68b65874a45b5732
Sergio <Sergioanca14@gmail.com>
2024-04-23 11:44:00 +0200

    Entregable 2 finalizado

--- Rest_Proyecto/src/main/java/es/us/lsi/dad/RestServer.java
+++ Rest_Proyecto/src/main/java/es/us/lsi/dad/RestServer.java
@@ -19,19 +19,20 @@ import io.vertx.ext.web.handler.BodyHandler;
 
 public class RestServer extends AbstractVerticle {
 
-	private Map<Integer, UserEntity> users = new HashMap<Integer, UserEntity>();
+	
+	/************************/
+	/*****INICIO REST********/
+	/************************/
+	
+	private Map<Integer, SensorTemperaturaEntity> temperaturas = new HashMap<Integer, SensorTemperaturaEntity>();
+	private Map<Integer, SensorLuzEntity> luces= new HashMap<Integer, SensorLuzEntity>();
 	private Gson gson;
-
+	
 	public void start(Promise<Void> startFuture) {
-		// Creating some synthetic data
-		createSomeData(25);
-
 		// Instantiating a Gson serialize object using specific date format
 		gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
-
 		// Defining the router object
 		Router router = Router.router(vertx);
-
 		// Handling any server startup result
 		vertx.createHttpServer().requestHandler(router::handle).listen(8084, result -> {
 			if (result.succeeded()) {
@@ -43,59 +44,73 @@ public class RestServer extends AbstractVerticle {
 
 		// Defining URI paths for each method in RESTful interface, including body
 		// handling by /api/users* or /api/users/*
-		router.route("/api/users*").handler(BodyHandler.create());
-		router.get("/api/users").handler(this::getAllWithParams);
-		router.get("/api/users/user/all").handler(this::getAll);
-		router.get("/api/users/:userid").handler(this::getOne);
-		router.post("/api/users").handler(this::addOne);
-		router.delete("/api/users/:userid").handler(this::deleteOne);
-		router.put("/api/users/:userid").handler(this::putOne);
+		router.route("/api/temperaturas*").handler(BodyHandler.create());
+		router.get("/api/temperaturas").handler(this::getAllWithParamsT);
+		router.get("/api/temperaturas/temperatura/allt").handler(this::getAllT);
+		router.get("/api/temperaturas/:idtempt").handler(this::getOneT);
+		router.post("/api/temperaturas").handler(this::addOneT);
+		router.delete("/api/temperaturas/:idtempt").handler(this::deleteOneT);
+		router.put("/api/temperaturas/:idtempt").handler(this::putOneT);
+		//TODO cambiar user id por luz/temperatura id, pero no se q id poner pq userid no esta en USerEntity
+		router.route("/api/luces*").handler(BodyHandler.create());
+		router.get("/api/luces").handler(this::getAllWithParamsL);
+		router.get("/api/luces/luz/alll").handler(this::getAllL);
+		router.get("/api/luces/:idluz").handler(this::getOneL);
+		router.post("/api/luces").handler(this::addOneL);
+		router.delete("/api/luces/:idluz").handler(this::deleteOneL);
+		router.put("/api/luces/:idluz").handler(this::putOneL);
 	}
-
+	
 	@Override
 	public void stop(Promise<Void> stopPromise) throws Exception {
 		try {
-			users.clear();
+			temperaturas.clear();
+			luces.clear();
 			stopPromise.complete();
 		} catch (Exception e) {
 			stopPromise.fail(e);
 		}
 		super.stop(stopPromise);
 	}
-
+	
+	/************************/
+	/*****TEMPERATURA********/
+	/************************/
+	
 	@SuppressWarnings("unused")
-	private void getAll(RoutingContext routingContext) {
+	private void getAllT(RoutingContext routingContext) {
 		routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(200)
-				.end(gson.toJson(new UserEntityListWrapper(users.values())));
+				.end(gson.toJson(new TemperaturaEntityListWrapper(temperaturas.values())));
 	}
-
-	private void getAllWithParams(RoutingContext routingContext) {
-		final String name = routingContext.queryParams().contains("name") ? routingContext.queryParam("name").get(0)
-				: null;
-		final String surname = routingContext.queryParams().contains("surname")
-				? routingContext.queryParam("surname").get(0)
-				: null;
-		final String username = routingContext.queryParams().contains("username")
-				? routingContext.queryParam("username").get(0)
-				: null;
-
+	
+	private void getAllWithParamsT(RoutingContext routingContext) {
+		
+		//TODO Comprobar qeu temperatura y timestamp son String y no Intefer y Long
+		final String temperatura = routingContext.queryParams().contains("temperatura") ? 
+				routingContext.queryParam("temperatura").get(0) : null;
+		
+		final String timestampt = routingContext.queryParams().contains("timestampt") 
+				? routingContext.queryParam("timestampt").get(0) : null;
+		
+		Integer temperaturaint = Integer.parseInt(temperatura);
+		Long timestamptlong = Long.parseLong(timestampt) ;
+		
 		routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(200)
-				.end(gson.toJson(new UserEntityListWrapper(users.values().stream().filter(elem -> {
+				.end(gson.toJson(new TemperaturaEntityListWrapper(temperaturas.values().stream().filter(elem -> {
 					boolean res = true;
-					res = res && (name != null ? elem.getName().equals(name) : true);
-					res = res && (surname != null ? elem.getSurname().equals(surname) : true);
-					res = res && (username != null ? elem.getUsername().equals(username) : true);
+					res = res && (temperaturaint != null ? elem.getTemperatura().equals(temperaturaint) : true);
+					res = res && (timestamptlong != null ? elem.getTimestampt().equals(timestamptlong) : true);
 					return res;
 				}).collect(Collectors.toList()))));
 	}
-
-	private void getOne(RoutingContext routingContext) {
+	
+	private void getOneT(RoutingContext routingContext) {
 		int id = 0;
 		try {
-			id = Integer.parseInt(routingContext.request().getParam("userid"));
+			id = Integer.parseInt(routingContext.request().getParam("idtempt"));
 
-			if (users.containsKey(id)) {
-				UserEntity ds = users.get(id);
+			if (temperaturas.containsKey(id)) {
+				SensorTemperaturaEntity ds = temperaturas.get(id);
 				routingContext.response().putHeader("content-type", "application/json; charset=utf-8")
 						.setStatusCode(200).end(ds != null ? gson.toJson(ds) : "");
 			} else {
@@ -107,19 +122,99 @@ public class RestServer extends AbstractVerticle {
 					.end();
 		}
 	}
-
-	private void addOne(RoutingContext routingContext) {
-		final UserEntity user = gson.fromJson(routingContext.getBodyAsString(), UserEntity.class);
-		users.put(user.getIdusers(), user);
+	
+	private void addOneT(RoutingContext routingContext) {
+		final SensorTemperaturaEntity temp = gson.fromJson(routingContext.getBodyAsString(), SensorTemperaturaEntity.class);
+		temperaturas.put(temp.getIdth(), temp);
 		routingContext.response().setStatusCode(201).putHeader("content-type", "application/json; charset=utf-8")
-				.end(gson.toJson(user));
+				.end(gson.toJson(temp));
+	}
+	
+	private void deleteOneT(RoutingContext routingContext) {
+		int id = Integer.parseInt(routingContext.request().getParam("idtempt"));
+		if (temperaturas.containsKey(id)) {
+			SensorTemperaturaEntity temp = temperaturas.get(id);
+			temperaturas.remove(id);
+			routingContext.response().setStatusCode(200).putHeader("content-type", "application/json; charset=utf-8")
+					.end(gson.toJson(temperaturas));
+		} else {
+			routingContext.response().setStatusCode(204).putHeader("content-type", "application/json; charset=utf-8")
+					.end();
+		}
+	}
+	private void putOneT(RoutingContext routingContext) {
+		int id = Integer.parseInt(routingContext.request().getParam("idtempt"));
+		SensorTemperaturaEntity ds = temperaturas.get(id);
+		final SensorTemperaturaEntity element = gson.fromJson(routingContext.getBodyAsString(), SensorTemperaturaEntity.class);
+		ds.setTemperatura(element.getTemperatura());
+		ds.setTimestampt(element.getTimestampt());
+		temperaturas.put(ds.getIdth(), ds);
+		routingContext.response().setStatusCode(201).putHeader("content-type", "application/json; charset=utf-8")
+				.end(gson.toJson(element));
+	}
+	
+	/****************/
+	/*****LUZ********/
+	/****************/
+	
+	@SuppressWarnings("unused")
+	private void getAllL(RoutingContext routingContext) {
+		routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(200)
+				.end(gson.toJson(new LuzEntityListWrapper(luces.values())));
+	}
+	private void getAllWithParamsL(RoutingContext routingContext) {
+		final String luz = routingContext.queryParams().contains("nivel_luz") ? 
+				routingContext.queryParam("nivel_luz").get(0) : null;
+		
+		final String timestampl = routingContext.queryParams().contains("timestampl") 
+				? routingContext.queryParam("timestampl").get(0) : null;
+		
+		Integer luzint = Integer.parseInt(luz);
+		Long timestamptllong = Long.parseLong(timestampl) ;
+		
+
+		routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(200)
+				.end(gson.toJson(new LuzEntityListWrapper(luces.values().stream().filter(elem -> {
+					boolean res = true;
+					res = res && (luzint != null ? elem.getNivel_luz().equals(luzint) : true);
+					res = res && (timestampl != null ? elem.getNivel_luz().equals(timestampl) : true);
+					return res;
+				}).collect(Collectors.toList()))));
 	}
 
-	private void deleteOne(RoutingContext routingContext) {
-		int id = Integer.parseInt(routingContext.request().getParam("userid"));
-		if (users.containsKey(id)) {
-			UserEntity user = users.get(id);
-			users.remove(id);
+	
+	private void getOneL(RoutingContext routingContext) {
+		int id = 0;
+		try {
+			id = Integer.parseInt(routingContext.request().getParam("idluz"));
+
+			if (luces.containsKey(id)) {
+				SensorLuzEntity ds = luces.get(id);
+				routingContext.response().putHeader("content-type", "application/json; charset=utf-8")
+						.setStatusCode(200).end(ds != null ? gson.toJson(ds) : "");
+			} else {
+				routingContext.response().putHeader("content-type", "application/json; charset=utf-8")
+						.setStatusCode(204).end();
+			}
+		} catch (Exception e) {
+			routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(204)
+					.end();
+		}
+	}
+	
+	private void addOneL(RoutingContext routingContext) {
+		final SensorLuzEntity luz = gson.fromJson(routingContext.getBodyAsString(), SensorLuzEntity.class);
+		luces.put(luz.getIdl(), luz);
+		routingContext.response().setStatusCode(201).putHeader("content-type", "application/json; charset=utf-8")
+				.end(gson.toJson(luz));
+	}
+	
+	
+	private void deleteOneL(RoutingContext routingContext) {
+		int id = Integer.parseInt(routingContext.request().getParam("idluz"));
+		if (luces.containsKey(id)) {
+			SensorLuzEntity user = luces.get(id);
+			luces.remove(id);
 			routingContext.response().setStatusCode(200).putHeader("content-type", "application/json; charset=utf-8")
 					.end(gson.toJson(user));
 		} else {
@@ -127,28 +222,20 @@ public class RestServer extends AbstractVerticle {
 					.end();
 		}
 	}
-
-	private void putOne(RoutingContext routingContext) {
+	
+	private void putOneL(RoutingContext routingContext) {
 		int id = Integer.parseInt(routingContext.request().getParam("userid"));
-		UserEntity ds = users.get(id);
-		final UserEntity element = gson.fromJson(routingContext.getBodyAsString(), UserEntity.class);
-		ds.setName(element.getName());
-		ds.setSurname(element.getSurname());
-		ds.setBirthdate(element.getBirthdate());
-		ds.setPassword(element.getPassword());
-		ds.setUsername(element.getUsername());
-		users.put(ds.getIdusers(), ds);
+		SensorLuzEntity ds = luces.get(id);
+		final SensorLuzEntity element = gson.fromJson(routingContext.getBodyAsString(), SensorLuzEntity.class);
+		ds.setNivel_luz(element.getNivel_luz());
+		ds.setTimestampl(element.getTimestampl());
+		luces.put(ds.getIdl(), ds);
 		routingContext.response().setStatusCode(201).putHeader("content-type", "application/json; charset=utf-8")
 				.end(gson.toJson(element));
 	}
-
-	private void createSomeData(int number) {
-		Random rnd = new Random();
-		IntStream.range(0, number).forEach(elem -> {
-			int id = rnd.nextInt();
-			users.put(id, new UserEntity(id, "Nombre_" + id, "Apellido_" + id,
-					new Date(Calendar.getInstance().getTimeInMillis() + id), "Username_" + id, "Password_" + id));
-		});
-	}
-
+	
+	/************************/
+	/*****FINALIZO REST******/
+	/************************/
+	
 }
---
+++ Rest_Proyecto/src/main/java/es/us/lsi/dad/TemperaturarEntityListWrapper.java
@@ -0,0 +1,13 @@
+package es.us.lsi.dad;
+
+import com.google.gson.JsonElement;
+
+public class TemperaturarEntityListWrapper extends JsonElement {
+
+	@Override
+	public JsonElement deepCopy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
